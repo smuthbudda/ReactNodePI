878f40843fc02314cca1d505c467297f68cf8773
smuthbudda <jkdsamson@outlook.com>
2022-08-07 21:55:25 +1000

    changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,17 @@
 module.exports = {
 	HOST: "localhost",
 	USER: "postgres",
-	PASSWORD, "123".
+	PASSWORD: "123",
 	DB: "testdb",
 	dialect: "postgres",
 	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000
+	  max: 5,
+	  min: 0,
+	  acquire: 30000,
+	  idle: 10000
 	}
-};
+  };
+
+  //max: maximum number in connection pool
+  //min:min number in connection pool
+  
--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const app = express();
 var corsOptions = {origin: "http://localhost:8081"};
 
 app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
 });
+
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log("server is running on port ${PORT}");
