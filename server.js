79c1896d099a66445e09bfbf3fa5ee49e64e93be
smuthbudda <jkdsamson@outlook.com>
2022-08-07 21:22:29 +1000

    Server changed

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const express = require ("express")
-const bodyParse
+const express = require ("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+var corsOptions = {origin: "http://localhost:8081"};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("server is running on port ${PORT}");
+});
